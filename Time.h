25f88ab7959c83ab08d163c3f776853b57016dd8
NTMTshali <213522460@stu.ukzn.ac.za>
2015-04-13 12:07:41 +0200

    Interface of time

---
+++ Time.h
@@ -0,0 +1,20 @@
+#ifndef TIME_H
+#define TIME_H
+#include < iostream>
+using namespace std;
+
+class Time
+{
+private:
+	int Hour;
+	int Min;
+
+private:
+	Time(int, int);
+	~Time();
+	void set(int, int);
+	void get(int, int) const;
+	friend ostream &operator<<(ostream &, const Time &);
+	Time operator++(int);
+};
+#endif
